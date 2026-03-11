e7496b1dc307c4f46e59e69ae385b312bf293730
junhyunsim0402 <simjjang11@gmail.com>
2026-03-11 12:52:55 +0900

    feat 1. 특정 매장 정보 조회 기능

--- src/main/java/com/team3/seatmaster/store/service/StoreService.java
+++ src/main/java/com/team3/seatmaster/store/service/StoreService.java
@@ -42,8 +42,26 @@ public class StoreService {
     public StoreDto getStore(int no){
         Optional<StoreEntity> optional=storeRepository.findById(no);
         if(optional.isPresent()){
+            StoreEntity entity=optional.get();
+            StoreDto storeDto=new StoreDto();
+            storeDto.setNo(entity.getNo());
+            storeDto.setOwner_no(entity.getOwner().getNo());
+            storeDto.setName(entity.getName());
+            storeDto.setCategory(entity.getCategory());
+            storeDto.setAddress(entity.getAddress());
+            storeDto.setContact(entity.getContact());
+            storeDto.setEmail(entity.getEmail());
+            storeDto.setBh_weekdays(entity.getBh_weekdays());
+            storeDto.setBh_saturday(entity.getBh_saturday());
+            storeDto.setBh_sunday(entity.getBh_sunday());
+            storeDto.setStatus(entity.getStatus());
+            storeDto.setCreate_date(entity.getCreateDate().toString());
+            storeDto.setUpdate_date(entity.getUpdateDate().toString());
+            return storeDto;
         }
+        return null;
     }
+
 
     // 매장 삭제
     public boolean deleteStore(int no){
